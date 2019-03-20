4d4fc4223b42cd610d83f2461dc9946960e57e70
Fadeness <15074868093@163.com>
2019-03-20 23:39:35 +0800

    Create GoogleTranslateEnhance.js

---
+++ GoogleTranslateEnhance.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         GoogleTranslateEnhance
+// @namespace    https://github.com/Fadeness/GoogleTranslateEnhance
+// @version      0.1
+// @description  enhance google translate's user experience
+// @author       Fadeness
+// @match        https://translate.google.cn/*
+// @match        https://translate.google.com/*
+// @grant        none
+// @require      https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const source = $("#source");
+
+    source.on("mouseenter", function() {
+        $(this).select();
+    })
+})();
